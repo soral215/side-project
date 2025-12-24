a8e019d6b3723518317b2a3e95690f1f0f214c74
soral215 <sora901215@gmail.com>
2025-12-24 17:01:55 +0900

    chore: update shared package to use ES module syntax by adding "type": "module" in package.json and updating import paths to include file extensions

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,15 +1,15 @@
 // 공통 타입 정의
-export * from './types';
+export * from './types/index.js';
 
 // 공통 유틸리티 함수
-export * from './utils';
+export * from './utils/index.js';
 
 // 검증 스키마
-export * from './validations';
+export * from './validations/index.js';
 
 // 명시적 export (tsx 호환성)
-export { createApiResponse, createErrorResponse, formatDate, formatZodError } from './utils';
-export type { User, ApiResponse, CreateUserRequest, UpdateUserRequest, PaginatedResponse, PaginationParams } from './types';
-export { createUserSchema, updateUserSchema, registerSchema, loginSchema, updateProfileSchema, changePasswordSchema, ZodError } from './validations';
-export type { CreateUserInput, UpdateUserInput, RegisterInput, LoginInput, UpdateProfileInput, ChangePasswordInput } from './validations';
+export { createApiResponse, createErrorResponse, formatDate, formatZodError } from './utils/index.js';
+export type { User, ApiResponse, CreateUserRequest, UpdateUserRequest, PaginatedResponse, PaginationParams } from './types/index.js';
+export { createUserSchema, updateUserSchema, registerSchema, loginSchema, updateProfileSchema, changePasswordSchema, ZodError } from './validations/index.js';
+export type { CreateUserInput, UpdateUserInput, RegisterInput, LoginInput, UpdateProfileInput, ChangePasswordInput } from './validations/index.js';
 
--- packages/shared/src/validations/index.ts
+++ packages/shared/src/validations/index.ts
@@ -1,5 +1,5 @@
-export * from './user.schema';
-export * from './auth.schema';
-export * from './post.schema';
+export * from './user.schema.js';
+export * from './auth.schema.js';
+export * from './post.schema.js';
 export { ZodError } from 'zod';
 
